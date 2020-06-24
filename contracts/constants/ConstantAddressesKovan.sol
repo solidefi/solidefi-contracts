996b9d78ba20df3ead87abfd06f74c062fc47df0
Surbhi Audichya <audichyasurbhi@gmail.com>
2020-06-24 21:29:56 +0530

    DS removed

--- contracts/constants/ConstantAddressesKovan.sol
+++ contracts/constants/ConstantAddressesKovan.sol
@@ -1,25 +1,12 @@
 pragma solidity >=0.6.0;
 
-
 contract ConstantAddressesKovan {
-    address public constant MAKER_DAI_ADDRESS = 0xC4375B7De8af5a38a93548eb8453a498222C4fF2;
-
     address public constant LOGGER_ADDRESS = 0xf1A6dA3d64F67c4A4800672836A4c5ebF4623473;
-    //0x582e80BD86CDF67F98530ac60600858Cb4B86F91
-
-    address public constant COMPOUND_DAI_ADDRESS = 0x25a01a05C188DaCBCf1D61Af55D4a5B4021F7eeD;
     address public constant SOLO_MARGIN_ADDRESS = 0x4EC3570cADaAEE08Ae384779B0f3A45EF85289DE;
 
-    address public constant CDAI_ADDRESS = 0xb6b09fBffBa6A5C4631e5F7B2e3Ee183aC259c0d;
-    // common ProxyRegistry
-    address public constant PROXY_REGISTRY_INTERFACE_ADDRESS = 0x64A436ae831C1672AE81F674CAb8B6775df3475C;
-
-    // new MCD contracts
     address public constant SAI_ADDRESS = 0xC4375B7De8af5a38a93548eb8453a498222C4fF2;
     address public constant DAI_ADDRESS = 0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa;
-
-    address public constant NEW_CDAI_ADDRESS = 0xe7bc397DBd069fC7d0109C0636d06888bb50668c;
-    address public constant NEW_IDAI_ADDRESS = 0x6c1E2B0f67e00c06c8e2BE7Dc681Ab785163fF4D;
+    address public constant CDAI_ADDRESS = 0xe7bc397DBd069fC7d0109C0636d06888bb50668c;
 
     // Aave
     address public constant LENDING_PROTO_ADDRESS_PROV = 0x506B0B2CF20FAA8f38a4E2B524EE43e1f4458Cc5;
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,34 +3,19 @@ const DydxProtocol = artifacts.require("./DydxProtocol.sol");
 const CompoundProtocol = artifacts.require("./CompoundProtocol");
 const AaveProtocol = artifacts.require("./AaveProtocol");
 const Logger = artifacts.require("./Logger.sol");
-//require('dotenv').config();
+require('dotenv').config();
 
 module.exports = function (deployer, network, accounts) {
-    // let deployAgain = (process.env.DEPLOY_AGAIN === 'true') ? true : false;
+    let deployAgain = (process.env.DEPLOY_AGAIN === 'true') ? true : false;
 
     deployer.then(async () => {
         // lend logger 
         //await deployer.deploy(Logger, { gas: 6000000 })
-        // --------- first deploy this part ---------------------------------
-
+       
         // await deployer.deploy(CompoundProtocol, { gas: 6000000 })
         // await deployer.deploy(DydxProtocol, { gas: 6000000 })
         // await deployer.deploy(AaveProtocol, { gas: 6000000 })
-
-
-
-        // --------- change addresses in protocolProxy contract and then deploy this part --------------
-
-        let dydxProtocol = await DydxProtocol.deployed()
-        let compoundProtocol = await CompoundProtocol.deployed()
-        let aaveProtocol = await AaveProtocol.deployed();
-
-        await deployer.deploy(ProtocolProxy, { gas: 6000000 })
-        let protocolProxy = await ProtocolProxy.deployed()
-
-        await dydxProtocol.addProtocolProxy(protocolProxy.address, { gas: 6000000 })
-        await compoundProtocol.addProtocolProxy(protocolProxy.address, { gas: 6000000 })
-        await aaveProtocol.addProtocolProxy(protocolProxy.address, { gas: 6000000 })
+         await deployer.deploy(ProtocolProxy, { gas: 6000000 })
 
     });
 };
