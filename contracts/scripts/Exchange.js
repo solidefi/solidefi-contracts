701e867b8caa07156eaccf2965abc69df9f4a658
Surbhi Audichya <audichyasurbhi@gmail.com>
2020-07-09 09:20:12 +0530

    synthetix exchange sUSD for sXAG using gnosis batching

---
+++ contracts/scripts/Exchange.js
@@ -0,0 +1,97 @@
+
+const synthetix = require('synthetix'); // nodejs
+//const ethers = require('ethers'); // nodejs
+const CPK = require('contract-proxy-kit')
+const Web3 = require('web3');
+
+
+const network = 'kovan';
+//const provider = ethers.getDefaultProvider(network === 'mainnet' ? 'homestead' : network);
+//const privateKey =  process.env.PRIV_KEY; // don't actually put a private key in code obviously
+//const signer = new ethers.Wallet(privateKey).connect(provider);
+
+web3 = new Web3(new Web3.providers.HttpProvider("https://kovan.infura.io/v3/11b9a3581295441e896cacb35648a968"));
+
+account = web3.eth.accounts.privateKeyToAccount("0xE1DDD8AAFC215C39C5CD6D35BB6EECF5B323ABF78FE566BB7A94FCC7479101C7")
+web3.eth.accounts.wallet.add(account)
+
+const  {address: addressSynthetix}  = synthetix.getTarget({ network, contract: 'ProxyERC20' });
+const  {abi : abiSynthetix}  = synthetix.getSource({ network, contract: 'Synthetix' });
+const Synthetix = new web3.eth.Contract(abiSynthetix,addressSynthetix);
+
+const { toBytes32 } = synthetix;
+
+
+const {  address: addressDepot} = synthetix.getTarget({ network, contract: 'Depot' });
+const  { abi: abiDepot }  = synthetix.getSource({ network, contract: 'Depot' });
+
+const Depot = new web3.eth.Contract(abiDepot,addressDepot);
+
+
+trading = (async () => {
+  try {
+   
+     const cpk = await CPK.create({ web3 , ownerAccount:account.address});
+    
+  // console.log(Synthetix);
+  const addressSUSD = await Synthetix.methods.synths(toBytes32('sUSD')).call();
+  //console.log(JSON.stringify(addressSUSD,  null, '\t'));
+  const sUSDContract = new web3.eth.Contract(abiSynthetix,addressSUSD);
+
+  let txnDepot = await Depot.methods.synthsReceivedForEther(web3.utils.toHex((0.001*1e18))).call();
+console.log(JSON.stringify(txnDepot,  null, '\t'));
+
+console.log()
+   const { promiEvent, hash } = await cpk.execTransactions([
+       {
+         operation: CPK.CALL,
+         to: Depot.options.address,
+         value: web3.utils.toHex(0.001*1e18),                           
+         data: Depot.methods.exchangeEtherForSynths().encodeABI(),
+       },
+       {
+        operation: CPK.CALL,
+        to: Synthetix.options.address,
+        value: 0,                           
+        data: Synthetix.methods.exchange(toBytes32('sUSD'), web3.utils.toHex((txnDepot)), toBytes32('sXAG')).encodeABI(),
+      },
+
+      ],{gasLimit: 999990});
+      console.log(hash);
+
+      
+   
+
+
+
+  
+  
+// /* quantity of sUSD received in exchange for a given quantity of ETH */
+// const txnDepot = await Depot.methods.synthsReceivedForEther(web3.utils.toHex((0.01*1e18))).call();
+// console.log(JSON.stringify(txnDepot,  null, '\t'));
+
+// /* Sells sUSD to callers who send ether */
+// const exchangeEtherForSynths = await Depot.methods.exchangeEtherForSynths().send({from:account.address,value: web3.utils.toHex((0.001*1e18)),gasLimit:9999990});
+// console.log(JSON.stringify(exchangeEtherForSynths,  null, '\t'));
+
+// /* exchange sUSD for sXAG */
+//   const txn = await Synthetix.methods.exchange(toBytes32('sUSD'), web3.utils.toHex((0.01*1e18)), toBytes32('sXAG')).send({from:account.address, gasLimit:9999990}).on('transactionHash', function(hash){
+//     console.log(hash);
+// })
+// .on('confirmation', function(confirmationNumber, receipt){
+//   if(confirmationNumber > 2 )return 
+// })
+// .on('receipt', function(receipt){
+//     // receipt example
+//     console.log(receipt);
+// }).on('error', function(error, receipt) { // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
+// console.log(error);
+// });
+
+
+
+  } catch (err) {
+    console.log('Error', err);
+  }
+})();
+
