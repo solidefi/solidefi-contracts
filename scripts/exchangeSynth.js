abc64d15262ef6c19c6fae018b186ca32d31f0c8
Surbhi Audichya <audichyasurbhi@gmail.com>
2020-07-21 19:02:36 +0530

    synthetix exchange DAI for synth

---
+++ scripts/exchangeSynth.js
@@ -0,0 +1,18 @@
+const request = require('request')
+const exchangeForSynth = (value, userAddress, callback) => {
+
+const url ='https://api.1inch.exchange/v1.1/swapQuote?fromTokenSymbol=DAI&toTokenSymbol=sUSD&amount=' + value + '&fromAddress=' + userAddress + '&slippage=1&disableEstimate=true'
+
+request({ url: url, json: true }, (error, response) => { 
+    if (error) {
+         callback('Unable to connect to services!', undefined) } 
+    else if (response.data === 0) 
+         {
+           callback('Unable to find data.',undefined)}   
+    else {
+           callback(undefined, {
+calldata: response.body.data, to: response.body.to, sUSDAmount: response.body.toTokenAmount
+}) }
+}) }
+module.exports = exchangeForSynth
+
