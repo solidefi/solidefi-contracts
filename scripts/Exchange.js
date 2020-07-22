da3354f8d71ab9bde48d5402b334503c7454ff8d
Surbhi Audichya <audichyasurbhi@gmail.com>
2020-07-22 19:08:26 +0530

    approved DAI to 1inch contract

--- scripts/Exchange.js
+++ scripts/Exchange.js
@@ -1,11 +1,9 @@
 const path = require("path");
 const dotenv = require('dotenv').config();
-const synthetix = require('synthetix'); // nodejs
-//const ethers = require('ethers'); // nodejs
+const synthetix = require('synthetix');
 const CPK = require('contract-proxy-kit')
 const Web3 = require('web3');
-var request = require("request")
-const rp = require('request-promise');
+
 const network = 'kovan';
 const exchangeForSynth = require("./exchangeSynth")
 
@@ -20,109 +18,64 @@ const Synthetix = new web3.eth.Contract(abiSynthetix,addressSynthetix);
 
 const { toBytes32 } = synthetix;
 
-
-const {  address: addressDepot} = synthetix.getTarget({ network, contract: 'Depot' });
-const  { abi: abiDepot }  = synthetix.getSource({ network, contract: 'Depot' });
-const Depot = new web3.eth.Contract(abiDepot,addressDepot);
-
-const {  address: addressDelegateApprovals} = synthetix.getTarget({ network, contract: 'DelegateApprovals' });
-const  { abi: abiDelegateApprovals }  = synthetix.getSource({ network, contract: 'DelegateApprovals' });
-const DelegateApprovals = new web3.eth.Contract(abiDelegateApprovals,addressDelegateApprovals);
-
 trading = (async () => {
   try {
    
   const cpk = await CPK.create({ web3 , ownerAccount:account.address});
   const addressSUSD = await Synthetix.methods.synths(toBytes32('sUSD')).call();
-
   const sUSDContract = new web3.eth.Contract(abiSynthetix,addressSUSD);
-
+  // const dai = new web3.eth.Contract(ERC20JSON.abi, DAI);
   const value = 0.001*1e18;
   
  
   exchangeForSynth(value, cpk.address, async(error, data) => { 
    
-    if (error) {
+     if (error) {
       return console.log(error) 
-    }
+     } 
      
-    const { promiEvent, hash } = await cpk.execTransactions([
-      {
-        operation: CPK.CALL,
-        to: data.to,
-        value: 0,                           
-        data: data.calldata,
-      },
-      {
-       operation: CPK.CALL,
-       to: Synthetix.options.address,
-       value: 0,                           
-       data: Synthetix.methods.exchange(toBytes32('sUSD'), web3.utils.toHex(data.sUSDAmount), toBytes32('sXAG')).encodeABI(),
-     },
+    //  const allowance = await dai.methods.allowance(accounts[0], data.to).call();
+    //  console.log("allowance ",allowance.toString());
+    //  if (allowance.toString() === '0') {
+    //   let gasLimit = await  dai.methods.approve( data.to,web3.utils.toWei(Number.MAX_SAFE_INTEGER.toString(), 'ether')).estimateGas({ from: accounts[0] });
+    //   let gasPrice = await web3.eth.getGasPrice();
+    //   await dai.methods.approve( data.to,web3.utils.toWei(Number.MAX_SAFE_INTEGER.toString(), 'ether')).send({ from: accounts[0],gasPrice: web3.utils.toHex(gasPrice),
+    //     gasLimit: web3.utils.toHex(gasLimit) });
+    // }
 
-     ],{gasLimit: 699990});
-     console.log(hash);
-})
-
-  // eth -> sUSD -> sToken 
-/*
-   let txnDepot = await Depot.methods.synthsReceivedForEther(web3.utils.toHex((0.012*1e18))).call();
-   console.log(JSON.stringify(txnDepot,  null, '\t'));
    
-//with gnosis 
-        await web3.eth.sendTransaction({
-          from: account.address,
-          to: '0x1bc2f452d95ffa5044a03c0804a379ed5b2bc0f5',
-          value: web3.utils.toHex(0.00001*1e18),
-          gasLimit: 999990
-        })
-  //if(!synthetix.isWaitingPeriod(src) 
-   const { promiEvent, hash } = await cpk.execTransactions([
+      try {
+        if(!await Synthetix.methods.isWaitingPeriod(toBytes32('sUSD'))){
+          return console.log(await Synthetix.methods.isWaitingPeriod(toBytes32('sUSD')).call());
+        }
+        console.log("sXAG",await Synthetix.methods.isWaitingPeriod(toBytes32('sXAG')).call());
+        console.log("sXAU",await Synthetix.methods.isWaitingPeriod(toBytes32('sXAU')).call());
+     const { promiEvent, hash } = await cpk.execTransactions([
        {
          operation: CPK.CALL,
-         to: Depot.options.address,
-         value: web3.utils.toHex(0.02*1e18),                           
-         data: Depot.methods.exchangeEtherForSynths().encodeABI(),
+         to: data.to,
+         value: 0,                           
+         data: data.calldata,
        },
        {
         operation: CPK.CALL,
         to: Synthetix.options.address,
         value: 0,                           
-        data: Synthetix.methods.exchange(toBytes32('sUSD'), web3.utils.toHex((txnDepot)), toBytes32('sXAG')).encodeABI(),
-      },
-
+        data: Synthetix.methods.exchange(toBytes32('sUSD'), web3.utils.toHex(data.sUSDAmount), toBytes32('sXAU')).encodeABI(),
+       },
       ],{gasLimit: 999990});
+      
       console.log(hash);
-
-    */
-  
-
-
-  //without gnosis batching 
-// /* quantity of sUSD received in exchange for a given quantity of ETH */
-// const txnDepot = await Depot.methods.synthsReceivedForEther(web3.utils.toHex((0.01*1e18))).call();
-// console.log(JSON.stringify(txnDepot,  null, '\t'));
-
-// /* Sells sUSD to callers who send ether */
-// const exchangeEtherForSynths = await Depot.methods.exchangeEtherForSynths().send({from:account.address,value: web3.utils.toHex((0.001*1e18)),gasLimit:9999990});
-// console.log(JSON.stringify(exchangeEtherForSynths,  null, '\t'));
-
-// /* exchange sUSD for sXAG */
-//   const txn = await Synthetix.methods.exchange(toBytes32('sUSD'), web3.utils.toHex((0.01*1e18)), toBytes32('sXAG')).send({from:account.address, gasLimit:9999990}).on('transactionHash', function(hash){
-//     console.log(hash);
-// })
-// .on('confirmation', function(confirmationNumber, receipt){
-//   if(confirmationNumber > 2 )return 
-// })
-// .on('receipt', function(receipt){
-//     // receipt example
-//     console.log(receipt);
-// }).on('error', function(error, receipt) { // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
-// console.log(error);
-// });
-
+    }  catch (err) {
+      console.log('Synthetix token Error', err);
+    }  
+    })
   } catch (err) {
     console.log('Error', err);
   }
 })();
 
+/**
+ * console.log( synthetix.getSuspensionReasons());
+ * Synthetix.methods.settle(toBytes32('sUSD'))
+ */
